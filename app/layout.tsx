b4e516b69eeeb9706ab3b7e89488b65e8afdef9b
Longbo <longboqiu@hotmail.com>
2025-01-02 22:16:12 +0100

    feat: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import Script from "next/script";
 
 const geistMono = Geist_Mono({
   subsets: ["latin"],
@@ -25,6 +26,15 @@ export const viewport = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-RKGM0H4TD9" strategy="afterInteractive" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RKGM0H4TD9');
+          `}
+      </Script>
       <body className={`${geistMono.className} antialiased`}>
         <div className="flex flex-col min-h-screen p-8 md:pt-16">
           <main className="max-w-2xl mx-auto w-full space-y-8">{children}</main>
