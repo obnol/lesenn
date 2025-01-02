d7cf34f5df169ef8ba2c028352c3d14f30ad78a1
Longbo <longboqiu@hotmail.com>
2025-01-02 11:25:09 +0100

    feat: add routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <p>hello world</p>;
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div className="flex justify-between">
+      <p>landing page</p>
+      <Link href="/login">login</Link>
+    </div>
+  );
 }
