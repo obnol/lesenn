1ffa4502e6881e5cb3fcc7f9040fdb92317a3bff
Longbo <longboqiu@hotmail.com>
2025-01-02 11:01:44 +0100

    feat: update supabase middleware

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -21,40 +21,39 @@ export async function updateSession(request: NextRequest) {
     },
   });
 
-  // IMPORTANT: Avoid writing any logic between createServerClient and
-  // supabase.auth.getUser(). A simple mistake could make it very hard to debug
-  // issues with users being randomly logged out.
-
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!user && !request.nextUrl.pathname.startsWith("/login") && !request.nextUrl.pathname.startsWith("/auth")) {
-    // no user, potentially respond by redirecting the user to the login page
+  const { pathname } = request.nextUrl;
+
+  // Define route types
+  const isLandingPage = pathname === "/";
+  const isLoginPage = pathname.startsWith("/login");
+  const isOnboardingPage = pathname.startsWith("/onboarding");
+  const isDynamicUserRoute = /^\/[^/]+(\/.*)?$/.test(pathname) && !isLandingPage && !isLoginPage && !isOnboardingPage;
+
+  // Redirect unauthorized users trying to access protected routes to `/`
+  if (!user && (isDynamicUserRoute || isOnboardingPage)) {
     const url = request.nextUrl.clone();
-    url.pathname = "/login";
+    url.pathname = "/";
     return NextResponse.redirect(url);
   }
 
-  if (request.nextUrl.pathname !== "/onboarding" && user && !user.user_metadata?.name) {
-    // user is logged in but has no name, redirect to onboarding
+  // Redirect users without a username to `/onboarding`
+  if (user && !user.user_metadata?.username && !isOnboardingPage) {
     const url = request.nextUrl.clone();
     url.pathname = "/onboarding";
     return NextResponse.redirect(url);
   }
 
-  // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
-  // creating a new response object with NextResponse.next() make sure to:
-  // 1. Pass the request in it, like so:
-  //    const myNewResponse = NextResponse.next({ request })
-  // 2. Copy over the cookies, like so:
-  //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
-  // 3. Change the myNewResponse object to fit your needs, but avoid changing
-  //    the cookies!
-  // 4. Finally:
-  //    return myNewResponse
-  // If this is not done, you may be causing the browser and server to go out
-  // of sync and terminate the user's session prematurely!
+  // Redirect logged-in users trying to access public routes to their dashboard `/[username]`
+  if (user && (isLandingPage || isLoginPage)) {
+    const url = request.nextUrl.clone();
+    url.pathname = `/${user.user_metadata?.username}`;
+    return NextResponse.redirect(url);
+  }
 
+  // Allow access to all other cases
   return supabaseResponse;
 }
