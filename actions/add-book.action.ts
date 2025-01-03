2f2f30b731e465c2e808224b56f9fee24963f9a7
Longbo <longboqiu@hotmail.com>
2025-01-03 18:12:12 +0100

    feat: add error handling in add book

--- actions/add-book.action.ts
+++ actions/add-book.action.ts
@@ -17,26 +17,30 @@ export const addBookAction = authActionClient
   .schema(addBookSchema)
   .metadata({ name: "add-book" })
   .action(async ({ parsedInput, ctx: { supabase, user } }) => {
-    const response = await fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=intitle:${encodeURIComponent(parsedInput.title)}&maxResults=1&key=${
-        process.env.NEXT_PUBLIC_GOOGLE_BOOKS_API_KEY
-      }`
-    );
-    const data = await response.json();
+    try {
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=intitle:${encodeURIComponent(parsedInput.title)}&maxResults=1&key=${
+          process.env.NEXT_PUBLIC_GOOGLE_BOOKS_API_KEY
+        }`
+      );
+      const data = await response.json();
 
-    if (!data.items) {
-      throw new Error("no book found");
+      if (!data.items) {
+        throw new Error("no book found");
+      }
+
+      const book = data.items[0] as GoogleBook;
+
+      await addBook(supabase, {
+        title: parsedInput.title,
+        isReading: parsedInput.isReading,
+        authors: book.volumeInfo.authors.map((author) => author.toLowerCase()),
+        userId: user.id,
+        pageCount: book.volumeInfo.pageCount,
+      });
+
+      revalidateTag(`user-library-${user.id}`);
+    } catch {
+      throw new Error("something went wrong please try again");
     }
-
-    const book = data.items[0] as GoogleBook;
-
-    await addBook(supabase, {
-      title: parsedInput.title,
-      isReading: parsedInput.isReading,
-      authors: book.volumeInfo.authors.map((author) => author.toLowerCase()),
-      userId: user.id,
-      pageCount: book.volumeInfo.pageCount,
-    });
-
-    revalidateTag(`user-library-${user.id}`);
   });
